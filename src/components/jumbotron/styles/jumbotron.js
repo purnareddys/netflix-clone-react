e4edf1c6a560f66fd6888295de211afe471a7ab3
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-08-20 00:16:56 +0530

    first commit

---
+++ src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Inner = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const Item = styled.div``;
+
+export const Container = styled.section``;
+
+export const Title = styled.h1``;
+
+export const SubTitle = styled.h2`
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+
+  @media (max-width: 600px) {
+    font-size: 18px;
+  }
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Jumbotron from "../components/jumbotron";
+
+export default function Home() {
+  return (
+    <Jumbotron.Container>
+      <p>This is home Fellas</p>
+    </Jumbotron.Container>
+  );
+}
